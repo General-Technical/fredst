e1fa71d2564fcdbf4343cabdfc6214ca4dbcb8c1
tm-ox <timothy@oxenfree.net>
2022-08-06 13:35:39 +0700

    Remove no https cdn

--- components/Head.vue
+++ components/Head.vue
@@ -6,6 +6,5 @@
     <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
     <Script src="https://unpkg.com/@alpinejs/intersect@3.x.x/dist/cdn.min.js" />
     <Script src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js" defer />
-    <Script src="/path/to/masonry.pkgd.min.js"></Script>
   </Head>
 </template>
