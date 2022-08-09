1c899b89152e8d495df1501db877f1f5a0914ce4
tm-ox <timothy@oxenfree.net>
2022-08-09 20:56:54 +0700

    Add CMS

--- pages/contact.vue
+++ pages/contact.vue
@@ -55,30 +55,3 @@ useHead({
   title: "Contact - Fred St.",
 });
 </script>
-
-<style scoped>
-.left {
-  @apply right-0 bg-gradient-to-r from-secondary to-transparent;
-}
-h1.left {
-  @apply ml-6 bg-none;
-}
-.squares.left {
-  @apply ml-6 mb-6 bg-none;
-}
-.content.left {
-  @apply bg-gradient-to-l from-secondary to-transparent border-l-0;
-}
-.right {
-  @apply ml-auto left-0 bg-gradient-to-l from-secondary to-transparent border-l-0;
-}
-h1.right {
-  @apply ml-auto mr-6 bg-none text-right;
-}
-.squares.right {
-  @apply justify-end mr-6 mb-6 bg-none;
-}
-.content.right {
-  @apply bg-gradient-to-r from-secondary to-transparent border-l-0 sm:border-l border-r-0;
-}
-</style>
