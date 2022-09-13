f7b59aa9e9d8db98cb40115a0350b0ae4b43e9d8
tm-ox <timothy@oxenfree.net>
2022-09-13 09:37:36 +0700

    Add Lightbox

---
+++ components/Lightbox copy.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <button @click="setActive" class="w-full">
+      <slot name="summary" />
+    </button>
+    <div
+      class="hidden site-padding trans fixed top-0 left-0 right-0 h-screen bottom-0 bg-secondary dark:bg-tertiary !bg-opacity-95 overflow-auto no-scrollbar z-50"
+      :class="{ active }"
+    >
+      <button
+        @click="setActive"
+        class="absolute right-0 top-6 hover:text-hover site-padding"
+      >
+        <FadeDown>
+          <Close />
+        </FadeDown>
+      </button>
+      <slot name="details" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      active: false,
+    };
+  },
+  methods: {
+    setActive() {
+      this.active = !this.active;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.active {
+  @apply block;
+}
+</style>
--- components/Lightbox.vue
+++ components/Lightbox.vue
@@ -5,18 +5,18 @@
     </button>
     <Teleport to="body">
       <div
-        class="hidden site-padding trans fixed top-0 left-0 right-0 h-screen bottom-0 bg-secondary dark:bg-tertiary !bg-opacity-95 z-50"
+        class="hidden site-padding trans fixed top-0 left-0 right-0 h-screen bottom-0 bg-secondary !bg-opacity-90 w-screen z-50"
         :class="{ active }"
       >
         <button
           @click="setActive"
-          class="absolute top-6 left-1/2 -translate-x-1/2 hover:text-hover site-padding"
+          class="absolute top-20 left-1/2 -translate-x-1/2 hover:text-accent site-padding"
         >
           <FadeDown>
             <Close />
           </FadeDown>
         </button>
-        <FadeUpLeave>
+        <FadeUpLeave class="mt-32">
           <slot name="details" />
         </FadeUpLeave>
       </div>
