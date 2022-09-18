11c139740c147f2849426f2e91e9a466e792fdc1
tm-ox <timothy@oxenfree.net>
2022-09-18 15:08:45 +0700

    Swiper

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,8 @@
-import { defineNuxtConfig } from 'nuxt';
+// import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ['@nuxt/content'],
-  colorMode: {
-    classSuffix: '',
-  },
   build: {
     postcss: {
       postcssOptions: {
