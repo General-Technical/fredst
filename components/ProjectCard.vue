787a919d813fd7da0e404c027138defc4a23df64
TM <tm@oxenfree.net>
2023-03-13 17:05:43 +0700

    sector/services project spacing

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -9,13 +9,13 @@
       {{ title }}
     </h4>
     <div class="flex flex-col border-t pt-3">
-      <div class="flex gap-3">
-        <h5 class="font-light">Sector:</h5>
-        <h5 class="font-thin">{{ sector }}</h5>
+      <div class="flex">
+        <h5 class="font-light project-subhead">Sector:</h5>
+        <h5 class="font-thin project-item">{{ sector }}</h5>
       </div>
-      <div class="flex gap-3">
-        <h5 class="font-light">Services:</h5>
-        <h5 class="font-thin">{{ services }}</h5>
+      <div class="flex">
+        <h5 class="font-light project-subhead">Services:</h5>
+        <h5 class="font-thin project-item">{{ services }}</h5>
       </div>
     </div>
     <ContentRenderer class="markdown !prose-sm my-3" :value="description" />
@@ -46,3 +46,12 @@ const props = defineProps({
   },
 });
 </script>
+
+<style>
+.project-subhead {
+  @apply w-1/5 sm:w-3/12 md:w-4/12 lg:w-2/6 xl:w-2/5 3xl:w-1/6;
+}
+.project-item {
+  @apply w-4/5 sm:w-9/12 md:w-9/12 xl:w-3/4 lg:w-3/6 3xl:w-5/6;
+}
+</style>
