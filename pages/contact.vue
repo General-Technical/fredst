8a8af17fb28898e9e116b8201f343d7a4a26a7a7
TM <tm@oxenfree.net>
2023-03-13 09:38:27 +0700

    fix climate columns + contact image

--- pages/contact.vue
+++ pages/contact.vue
@@ -13,16 +13,18 @@
         <div class="crd fade-up">
           <h1 class="page-title">{{ doc.title }}</h1>
           <Squares class="squares" />
-          <div class="flex flex-col md:flex-row mt-">
-            <div class="w-2/3">
+          <div class="grid grid-cols-1 md:grid-cols-5 md:gap-4">
+            <div class="md:col-span-2">
               <h3>FRED St PTY LTD</h3>
               <ContentRenderer :value="doc" class="markdown" />
             </div>
+            <!-- <div class="col-span-3"> -->
             <img
               :src="doc.map"
               :alt="doc.title"
-              class="w-full md:w-2/3 rounded"
+              class="w-full md:col-span-3 rounded"
             />
+            <!-- </div> -->
           </div>
           <p class="mt-4">
             you can follow us on the following socials:
